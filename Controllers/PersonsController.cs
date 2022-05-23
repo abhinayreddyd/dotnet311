445c4e27ee972a1df6ed137be09d4c2d6ce59d1f
abhinayreddyd <abhinayreddyd7@gmail.com>
2022-05-23 18:22:17 -0400

    view and connection changes
    view and connection changes

--- Controllers/PersonsController.cs
+++ Controllers/PersonsController.cs
@@ -53,7 +53,7 @@ namespace sampledb31.Controllers
         // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,LastName,FirstName,Age,City")] Persons persons)
+        public async Task<IActionResult> Create([Bind("LastName,FirstName,Age,City")] Persons persons)
         {
             if (ModelState.IsValid)
             {
